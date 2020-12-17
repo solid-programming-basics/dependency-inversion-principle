9c2a21af5129327735f203c01fd1d12262a2608d
Tomasz Pedziwiatr <tpedziwiatr@worldremit.com>
2020-12-17 18:46:54 +0100

    changes

--- src/main/java/edu/agh/wfiis/solid/dip/UserWebsiteVisitsCountRepository.java
+++ src/main/java/edu/agh/wfiis/solid/dip/UserWebsiteVisitsCountRepository.java
@@ -1,2 +1,26 @@
-package edu.agh.wfiis.solid.dip;public interface UserWebsiteVisitsCountRepository {
+package edu.agh.wfiis.solid.dip;
+
+import java.net.URL;
+import java.util.Date;
+
+public interface UserWebsiteVisitsCountRepository {
+    OperationResult save(UserWebsiteVisitsCount counts);
+    int read(String username, URL url, Date date);
+}
+
+class UserWebsiteVisitsCount {
+    String username;
+    URL url;
+    Date date;
+    int count;
+}
+
+class OperationResult {
+    public final String result;
+    public final boolean isSuccessful;
+
+    public OperationResult(String result, boolean isSuccessful) {
+        this.result = result;
+        this.isSuccessful = isSuccessful;
+    }
 }
