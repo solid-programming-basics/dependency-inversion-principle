bc62cb9d09aedefa1596f265de223242a954a19b
mrybak-wr <mrybak@worldremit.com>
2020-12-23 13:00:08 +0100

    adding repository interface

--- src/main/java/edu/agh/wfiis/solid/dip/UserWebsiteVisitsCountRepository.java
+++ src/main/java/edu/agh/wfiis/solid/dip/UserWebsiteVisitsCountRepository.java
@@ -1,26 +1,9 @@
-package edu.agh.wfiis.solid.dip;
-
-import java.net.URL;
 import java.util.Date;
 
 public interface UserWebsiteVisitsCountRepository {
-    OperationResult save(UserWebsiteVisitsCount counts);
-    int read(String username, URL url, Date date);
-}
 
-class UserWebsiteVisitsCount {
-    String username;
-    URL url;
-    Date date;
-    int count;
-}
+    int read(String username, Date visitDate);
 
-class OperationResult {
-    public final String result;
-    public final boolean isSuccessful;
+    void save(String username, Date visitDate, int visitsCount);
 
-    public OperationResult(String result, boolean isSuccessful) {
-        this.result = result;
-        this.isSuccessful = isSuccessful;
-    }
 }
