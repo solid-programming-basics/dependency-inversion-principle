0698d63c02fa32d6a73f5550ea727ee8762134f3
Adrian Furman <fadikk367@gmail.com>
2021-01-14 17:09:21 +0100

    Implement basic save and read operations for mongodb repository

--- src/main/java/edu/agh/wfiis/solid/dip/UserWebsiteVisitsCountMongoDBRepository.java
+++ src/main/java/edu/agh/wfiis/solid/dip/UserWebsiteVisitsCountMongoDBRepository.java
@@ -3,19 +3,52 @@ package edu.agh.wfiis.solid.dip;
 import com.mongodb.DB;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCollection;
+import com.mongodb.MongoDatabase;
+import com.mongodb.MongoException;
 
-public class UserWebsiteVisitsCountMongoDBRepository implements UserWebsiteVisitsCountRepository{
+import org.bson.Document;
+import static com.mongodb.client.model.Filters.*;
+
+
+public class UserWebsiteVisitsCountMongoDBRepository implements UserWebsiteVisitsCountRepository {
+    private MongoCollection<Document> userWebsiteVisitsCountCollection;
 
     public UserWebsiteVisitsCountMongoDBRepository() {
+        try (MongoClient mongoClient = MongoClients.create(System.getProperty("mongodb.uri"))) {
+
+            MongoDatabase db = mongoClient.getDatabase("solid_dependency_inversion");
+            this.userWebsiteVisitsCountCollection = db.getCollection("user_website_visits_count");
+        }
     }
 
     @Override
     public void save(UserWebsiteVisitsCount counts) throws OperationException {
+        var userWebsiteVisitsCount = new Document()
+            .append("username", counts.id.username)
+            .append("url", counts.id.url)
+            .append("date", counts.id.date)
+            .append("count", counts.count);
 
+        try {
+            this.userWebsiteVisitsCountCollection.insertOne(userWebsiteVisitsCount);
+        } catch(MongoException e) {
+            throw new OperationException(e.getMessage());
+        }
     }
 
     @Override
     public int read(UserWebsiteVisitsId id) throws OperationException {
-        return 0;
+        try {
+            Document foundUserWebsiteVisitsCount = this.userWebsiteVisitsCountCollection.find(and(
+                eq("username", id.username), 
+                eq("url", id.url), 
+                eq("date", id.date)
+            )).first();
+
+            return (int) foundUserWebsiteVisitsCount.get("count");
+        } catch(MongoException e) {
+            throw new OperationException(e.getMessage());
+        }
     }
 }
